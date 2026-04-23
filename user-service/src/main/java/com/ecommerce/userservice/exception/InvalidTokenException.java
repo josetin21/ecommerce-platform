707cc0dfb922b2058a8820d787f70af3eff5afb5
josetin21 <josetin2001@gmail.com>
2026-04-24 00:40:10 +0530

    feat: add custom exceptions and global exception handler

---
+++ user-service/src/main/java/com/ecommerce/userservice/exception/InvalidTokenException.java
@@ -0,0 +1,9 @@
+package com.ecommerce.userservice.exception;
+
+import org.springframework.http.HttpStatus;
+
+public class InvalidTokenException extends BaseException{
+    public InvalidTokenException(String message){
+        super(message, HttpStatus.UNAUTHORIZED);
+    }
+}
