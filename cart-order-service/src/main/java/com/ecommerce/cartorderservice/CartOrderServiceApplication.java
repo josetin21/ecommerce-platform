6f52dc1e4218288605672f907956f9744b4c33b1
josetin21 <josetin2001@gmail.com>
2026-05-03 12:56:10 +0530

    chore: add cart-order-service module with base setup, config and RabbitMQ

---
+++ cart-order-service/src/main/java/com/ecommerce/cartorderservice/CartOrderServiceApplication.java
@@ -0,0 +1,14 @@
+package com.ecommerce.cartorderservice;
+
+import com.ecommerce.cartorderservice.config.JwtProperties;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+@SpringBootApplication
+@EnableConfigurationProperties(JwtProperties.class)
+public class CartOrderServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CartOrderServiceApplication.class, args);
+    }
+}
