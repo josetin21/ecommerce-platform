361fd152247e6b5266da1f19b2b60261b0cdf118
josetin21 <josetin2001@gmail.com>
2026-07-14 14:08:19 +0530

    feat: sync order status on payment success/failure via RabbitMQ
    - add PAYMENT_FAILED to OrderStatus enum
    - consume PaymentSuccessEvent/PaymentFailedEvent in cart-order-service
    - update order status to CONFIRMED/PAYMENT_FAILED accordingly

---
+++ cart-order-service/src/main/java/com/ecommerce/cartorderservice/dto/event/PaymentSuccessEvent.java
@@ -0,0 +1,21 @@
+package com.ecommerce.cartorderservice.dto.event;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class PaymentSuccessEvent {
+    private UUID orderId;
+    private UUID paymentId;
+    private String razorpayPaymentId;
+    private BigDecimal amount;
+    private String userEmail;
+}
